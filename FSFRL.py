f4c40b83cdc0e46a1664c82e903631ab9da2ac87
Cauchemare <812145142@qq.com>
2018-06-06 11:52:55 +0800

    FSFRL_debug

--- FSFRL.py
+++ FSFRL.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu May 31 16:09:34 2018
@@ -36,17 +37,49 @@ def c_ensemble(df):
     return np.all([c_missing(df,df_missing),c_variance(df,df_missing,threshold=0.0)])
 
 
-class FSFR(BaseEstimator,TransformerMixin):  #FEATURE SELECTION FROM RAW_DATA
-    def __init__(self,typ_cols= None,u_cols=None,miss_rate=0.9):
+class FSFR(BaseEstimator,TransformerMixin):
+    """Feature selection from raw_data using lightgbm model
+            Args:
+                miss_rte:float,0-1;maximum rate of missing allowed
+                u_cols:list of columns names;usable columns names;
+                type_cols:dict or  mapping type ; variable sets {'cont_cols':list,'ordi_cols':list,'bina_cols':list,'norm_cols':list},
+                      keys name:{'cont_cols','ordi_cols','norm_cols','bina_cols'},not necessary to  include all keys name
+                      
+                y_dist:str,{regression, regression_l1, huber, fair, poisson, quantile, mape, gammma, tweedie, binary, multiclass, 
+                            multiclassova, xentropy, xentlambda, lambdarank} default=binary ;distribution of  y  
+                            detail see lightgbm document : http://lightgbm.readthedocs.io/en/latest/Parameters.html
+                metric:str or set of strs,{mean_absolute_error,mean_squared_error,root_mean_squared_error,mean_absolute_percentage_error,huber,
+                            fair,poisson,mean_average_precision,auc,binary_logloss,binary_error,multi_logloss,multi_error};
+                            
+                  
+     Attributes:
+        use_cols:list of columns names ; columns satisfy missing condition
+        tC_cols:list of columns names ; true continous variables set from  object dtype columns estimated
+        tO_cols:list of columns names ; true object variables set from  object dtype columns estimated
+                
+        gbm:booster;The trained Booster model;see detail http://lightgbm.readthedocs.io/en/latest/Python-API.html#training-api
+            used for plotting or predicting.
+
+        
+        s_cols:list of columns names;selected columns based on feature_importance of trained booster model
+                a rule of thumb:choose feature_importance>2 columns;
+
+    """
+    def __init__(self,typ_cols= None,u_cols=None,miss_rate=0.9,y_dist='binary',metric='auc'):
         self.miss_rte=miss_rate
         self.u_cols=u_cols
         self.type_cols=typ_cols
+        self.y_dist=y_dist
+        self.metric=metric
+        
     
     def  fit(self,X,Y):
         return  self._fit(X,Y)
+    
     def _fit(self,X,Y):
         global  miss_cnt,obs
         obs=len(X)
+        
         miss_cnt= self.miss_rte
         
         if self.u_cols is not None:
@@ -55,31 +88,28 @@ class FSFR(BaseEstimator,TransformerMixin):  #FEATURE SELECTION FROM RAW_DATA
         
         self.use_cols=X.apply(c_missing)[lambda i:i== True].index
 
-        #只保留use_cols
-        X_s=X.reindex(columns=self.use_cols)
-        o_cols=X_s.select_dtypes(include=['object']).columns
+
+        X_s=X.reindex(columns=self.use_cols)         #retain use_cols
+        o_cols=X_s.select_dtypes(include=['object']).columns  #find object dtype columns
         
+        self.tC_cols=np.compress(np.isin(o_cols,self.type_cols['cont_cols']),o_cols) 
+        X_s.loc[:,self.tC_cols]=X_s.loc[:,self.tC_cols].astype(np.float16)  #convert continous variables in o_cols to np.float16
         
-        #将o_cols中 continuous 变量 转换dtype
-        self.c_cols=np.compress(np.isin(o_cols,self.type_cols['cont_cols']),o_cols)
-        X_s.loc[:,self.c_cols]=X_s.loc[:,self.c_cols].astype(np.float16)
-        
-        #o_cols 中不属于c_conls 得变量为o2_cols
-        self.o2_cols=np.compress(np.isin(o_cols,self.type_cols['cont_cols'],invert=True),o_cols)
-        #对o2_cols 进行dummies
-        X_s=pd.get_dummies(X_s,columns=self.o2_cols)
+        self.tO_cols=np.compress(np.isin(o_cols,self.type_cols['cont_cols'],invert=True),o_cols) #fetch tO_cols,not continuous variables in o_cols
+
+        X_s=pd.get_dummies(X_s,columns=self.tO_cols)
    
-        self.use_cols2=X_s.apply(c_ensemble)[lambda i:i== True].index
+        use_cols_f=X_s.apply(c_ensemble)[lambda i:i== True].index
         
         
-        X_s=X_s.reindex(columns=self.use_cols2)
+        X_s=X_s.reindex(columns=use_cols_f)
         
         
         params = {
         'task': 'train',
         'boosting_type': 'gbdt',
-        'objective': 'binary',
-        'metric': 'auc',
+        'objective': self.y_dist,
+        'metric': self.metric,
         'num_leaves': 31,
         'learning_rate': 0.05,
         'feature_fraction': 0.9,
@@ -90,51 +120,48 @@ class FSFR(BaseEstimator,TransformerMixin):  #FEATURE SELECTION FROM RAW_DATA
         'is_sparse':True
             }
         
-        Y=Y.values.astype(np.int8).ravel()
+        if self.y_dist in ('binary', 'multiclass', 'multiclassova'):
+            Y=Y.values.astype(np.int8).ravel()
+            
         features=X_s.columns.tolist()
         X_d_matrix=csr_matrix(X_s.values)
+        
         del  X_s
         
         
         train_data=lgb.Dataset(X_d_matrix,Y,feature_name=features,params=params)
-#        print('Begin constructing datasets')
-        train_data.construct()  #提前优化数据集
+        train_data.construct()  
         del  X_d_matrix,Y
-#        print('Start fitting estimator')
         
-        gbm = lgb.train(params,
+        self.gbm = lgb.train(params,
                         train_data,
                         num_boost_round=4
                        )
-        lgb.plot_importance(gbm,max_num_features=10)
+
         
-        self.s_cols=np.compress(gbm.feature_importance()>2,gbm.feature_name())   #2<feature_importance<4.5
+        self.s_cols=np.compress(self.gbm.feature_importance()>2,self.gbm.feature_name())   
         
 #        print('s_cols corrwith y corr_coef> 0.1',X_s.reindex(columns=self.s_cols).corrwith(y_train).loc[lambda x :x >0.1])
         
         return self
+    
     def transform(self,X):
         X_s=X.reindex(columns=self.use_cols)
-        X_s.loc[:,self.c_cols]=X_s.loc[:,self.c_cols].astype(np.float16)
-        X_s=pd.get_dummies(X_s,columns=self.o2_cols)
-        X_s.rename(columns=partial(re.sub,'\s','_'),inplace=True)   #lgb.Data默认将所有变量空格填补为_,
+        X_s.loc[:,self.tC_cols]=X_s.loc[:,self.tC_cols].astype(np.float16)
+        X_s=pd.get_dummies(X_s,columns=self.tO_cols)
+        
+        X_s.rename(columns=partial(re.sub,'\s','_'),inplace=True)   #lightgbm convert implicitlyblank in column name to '_'  connector
         
         
         X_s=X_s.reindex(columns=self.s_cols)
-#        print ('X not find columns:',self.s_cols[np.all(pd.isnull(X_s),axis=0)])        
+        
+        not_finding_columns=self.s_cols[np.all(pd.isnull(X_s),axis=0)]
+        
+        if len(not_finding_columns)>0:
+             print ('X not find columns:',not_finding_columns)        
 
         return X_s
     
-    
-
-
-#FSFR 是否添加u_cols 与最后score没有关系，使用全部变量稍微提高score（17998 rows *2840 --> *5484 cols）  0.6164113344250406--> 0.6166009683706444
-    #不过速度更快：80.753601s -> 140.750888s
-
-#数据集越大，score 越高,当训练样本超过3W,score几乎不变，数据越大，s/rows*2840 速度越慢，因为内存不够需要释放空间
-# train_size= 0.6  29998 rows -> 23998 rows->17998 rows *2840cols  :0.63447->0.62357->0.616
-                      #129.169 s ->  96s --> 65s  0.004s/rows * 2840 cols
-                      #39998 rows*2840 cols  269.7252 s train_size=0.8 ->0.6350
 
      
         
